366836aa61b1df990e021bc867b13cb7e58f8cfc
stuck <aireportyt@gmail.com>
2024-03-20 14:04:41 +0100

    Add calendar heading and date highlight

--- routes/index.js
+++ routes/index.js
@@ -37,10 +37,19 @@ router.get('/', function(req, res, next) {
   let elevation = model.getElevation(date, lat, long)
   console.log(date, lat, long, elevation, maxElevation)
 
+  res.locals.date = date
+  res.locals.lat = lat
+  res.locals.long = long
+  res.locals.maxElevation = maxElevation
+  res.locals.elevation = elevation
+
   let graph_y = 50 + (20/6) * Math.max(0, maxElevation)
   let calendar = makeCalendar(date)
   res.locals.graph_y = graph_y
   res.locals.calendar = calendar
+
+  res.locals.day = date.getDate()
+  res.locals.month_s = date.toLocaleString(undefined, { month: "long" })
 
   res.render('index', site);
 });
