2906fc9da9ddf22e1178b4200ef2542a6b9ca1a3
stuck <aireportyt@gmail.com>
2024-03-29 20:40:19 +0100

    Add internationalization

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var createError = require('http-errors');
+var i18n = require('i18n-express');
 
 var indexRouter = require('./routes/index');
 
@@ -18,13 +19,29 @@ const LOGFILE = process.env.LOGFILE || '/tmp/sunwatch_access.log';
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// logging stuff
+// logging
 app.use(require('./logging')(logger, LOGFILE));
+
+// get variable handling
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(cookieParser());
+
+// static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+// localization
+app.use(i18n(
+  {
+    translationsPath: path.join(__dirname, 'lang'),
+    siteLangs: ['en', 'it'],
+    textsVarName: 'locale',
+    defaultLang: 'en',
+    browserEnable: true,
+    paramLangName: 'lang'
+  }
+))
 
 // index page
 app.use('/', indexRouter);
